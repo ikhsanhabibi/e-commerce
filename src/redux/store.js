5de0212c3050cffc7d77a0cc5cb333eaf613553c
Ikhsan Habibi <33756873+ikhsanhabibi@users.noreply.github.com>
2020-03-13 00:46:24 +0100

    no message

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,8 +7,6 @@ import rootReducer from "./root-reducer";
 const middlewares = [];
 
 if (process.env.NODE_ENV === `development`) {
-  const { logger } = require(`redux-logger`);
-
   middlewares.push(logger);
 }
 
